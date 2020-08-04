b9f60a051dbd9ec8bc6e17ee4ad69ee663369642
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-08-04 14:57:53 +0300

    Fixed percentage, but can be refactored.

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,7 +2,17 @@ import request from 'supertest';
 import app from '../app';
 
 describe('basic functionality', () => {
-  test('Should return OK', () => {
-    return request(app).get('/').expect(200);
+  test('Should fail if payload is empty', () => {
+    return request(app).post('/calculate').expect(400);
+  });
+
+  test('Should fail if payload is empty', () => {
+    return request(app).post('/calculate')
+      .send({expression: ["2"]})
+      .end(function(err, res) {
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.body).to.be.equal({result: '2'});
+        done();
+      });
   });
 });
--- src/controllers/Calculate.js
+++ src/controllers/Calculate.js
@@ -4,10 +4,17 @@ export default function Calculate(expression) {
   const stack = new ExpressionStack();
 
   for (let i = 0; i < expression.length; i++) {
+
+    if (expression[i].includes('%')) {
+      stack.stack.push('(' + expression[i].replace('%', ' / 100 ') + ')');
+      continue;
+    }
     stack.push(expression[i]);
   }
 
   try {
+    console.log('stack', stack.getStack());
+    console.log('convertForCalculation', stack.convertForCalculation());
     const result = stack.calculate();
     return {result: result};
   } catch (error) {
