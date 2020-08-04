e338d32da2cae4f0b0afcb080deee8ad4ec89162
limecodes <yan.shcherbakov+limecodes@gmail.com>
2020-08-04 08:38:59 +0300

    Non-escaping route input for now.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
+import cors from 'cors';
 
 import Calculate from './controllers/Calculate';
 
 const app = express();
 
+const corsOptions = {
+  origin: 'http://locahost:3000',
+  optionsSuccessStatus: 200,
+}
+
 app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.post('/calculate', [
   body('expression')
     .not().isEmpty().withMessage('Cannot be empty')
-    .trim()
-    .escape(),
+    .trim(),
 ], (req, res) => {
   // res.writeHead(200, {'Content-Type': 'application/json'});
   const errors = validationResult(req);
